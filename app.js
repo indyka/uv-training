32a5f3a428bd62027edf3d7809dcb302bb54a730
Indyka Saidi <indyka@ultravoucher.co.id>
2019-11-22 06:59:29 +0000

    add hello

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+// hello
+
 app.get('/', (req, res) => res.send('Zaki'));
 app.get('/test', (req, res) => res.send('Puguh'));
 app.get('/test2', (req, res) => res.send('Bonta'));
